2408ac59a3f974a891b8ae9016cc976bfd530500
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-05-01 16:48:14 +0700

    Update web.php

--- view/view/routes/web.php
+++ view/view/routes/web.php
@@ -16,3 +16,56 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// route mahasiswa
+Route::get('mahasiswa', function () {
+    return view('universitas.mahasiswa');
+});
+
+// send data to view as argument
+Route::get('mahasiswa', function () {
+    $arrMahasiswa = [
+        "Risa Lestari",
+        "Rudi Hermawan",
+        "Bambang Kusumo",
+        "Lisa Permata"
+    ];
+    return view('universitas.mahasiswa', ['mahasiswa' => $arrMahasiswa]);
+});
+
+// send data to view using with
+Route::get('mahasiswa', function () {
+    $arrMahasiswa = [
+        "Risa Lestari",
+        "Rudi Hermawan",
+        "Bambang Kusumo",
+        "Lisa Permata"
+    ];
+    return view('universitas.mahasiswa')->with('mahasiswa', $arrMahasiswa);
+});
+
+// send data using compact
+// Route::get('mahasiswa', function () {
+//     $mahasiswa01 = "Risa Lestari";
+//     $mahasiswa02 = "Rudi Hermawan";
+//     $mahasiswa03 = "Bambang Kusumo";
+//     $mahasiswa04 = "Lisa Permata";
+//     return view('universitas.mahasiswa')->with($mahasiswa01, $mahasiswa02, $mahasiswa03,$mahasiswa04);
+// });
+
+// // exercise
+// Route::prefix('mahasiswa')->group(function(){
+// Route::get(
+//     'mahasiswa/{nama}/{umur}/{daerah}',
+//     function ($nama, $umur, $daerah) {
+//         // $namaMahasiswa = $nama;
+//         // $umurMahasiswa = $umur;
+//         // $daerahMahasiswa = $daerah;
+//         return view('universitas.mahasiswa')->with(compact('nama','umur','daerah'));
+//         // return view('universitas.mahasiswa')
+//         //     ->with('nama', $nama)
+//         //     ->with('umur', $umur)
+//         //     ->with('daerah', $daerah);
+//     }
+// );
+// });
