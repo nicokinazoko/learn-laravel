6a429e05af6e51dcfa5ce089451290d021dd363d
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-05-03 17:21:45 +0700

    Update web.php

--- blade/blade/routes/web.php
+++ blade/blade/routes/web.php
@@ -16,3 +16,42 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/mahasiswa', function () {
+    $nama = 'Tya Kirana Putri';
+    $nilai = 75;
+    return view('mahasiswa', compact('nama', 'nilai'));
+});
+
+// foreach
+Route::get('/mahasiswa', function () {
+    $nama = 'Tya Kirana Putri';
+    $nilai = [80, 64, 30, 76, 95];
+    return view('mahasiswa', compact('nama', 'nilai'));
+});
+
+// design layout
+// mahasiswa Route
+Route::get('mahasiswa', function () {
+    $arrMahasiswa = [
+        "Risa Lestari", "Rudi Hermawan", "Bambang Kusumo", "Lisa Permata"
+    ];
+    return view('mahasiswa')->with('mahasiswa', $arrMahasiswa);
+});
+
+
+// dosen Route
+Route::get('dosen', function () {
+    $arrDosen =  ["Maya Fitrianti, M.M.", "Prof. Silvia Nst, M.Farm.", "Dr. Umar Agustinus", "Dr. Syahrial, M.Kom."];
+    return view('dosen')->with('dosen', $arrDosen);
+});
+
+// galeri Route
+Route::get('gallery', function () {
+    return view('galeri');
+});
+
+
+Route::get('admin', function () {
+    return view('admin');
+});
