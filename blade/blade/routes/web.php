48f47d3f46e2758951577b65d3951a1005a58764
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-05-08 16:54:01 +0700

    Update web.php

--- blade/blade/routes/web.php
+++ blade/blade/routes/web.php
@@ -37,21 +37,28 @@ Route::get('mahasiswa', function () {
         "Risa Lestari", "Rudi Hermawan", "Bambang Kusumo", "Lisa Permata"
     ];
     return view('mahasiswa')->with('mahasiswa', $arrMahasiswa);
-});
+})->name('mahasiswa');
 
 
 // dosen Route
 Route::get('dosen', function () {
     $arrDosen =  ["Maya Fitrianti, M.M.", "Prof. Silvia Nst, M.Farm.", "Dr. Umar Agustinus", "Dr. Syahrial, M.Kom."];
     return view('dosen')->with('dosen', $arrDosen);
-});
+})->name('dosen');
 
 // galeri Route
 Route::get('gallery', function () {
     return view('galeri');
-});
+})->name('galeri');
 
 
-Route::get('admin', function () {
+// named route
+Route::get('universitas/fmipa/matematika/gallery', function () {
     return view('admin');
-});
+})->name('gambar');
+
+// named route with parameter
+Route::get('informasi/{fakultas}/{jurusan}', function ($fakultas,$jurusan) {
+    $data = [$fakultas,$jurusan];
+    return view('informasi')->with('data',$data);
+})->name('informasi');
