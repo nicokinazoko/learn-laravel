0fa8e09578d907f9a653c15420b5d59a85a86f63
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-05-15 16:34:50 +0700

    Create PageController.php

---
+++ laravel-ui/laravel-ui/app/Http/Controllers/PageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function tampil()
+    {
+        $arrMahasiswa = [
+            "Risa Lestari", "Rudi Hermawan", "Bambang Kusumo",
+            "Lisa Permata"
+        ];
+
+        return view('mahasiswa')->with('mahasiswa', $arrMahasiswa);
+    }
+}
