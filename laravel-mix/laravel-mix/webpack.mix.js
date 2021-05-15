cf9b6b9821476591909181c092b7bafe5ca1212e
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-05-15 16:14:52 +0700

    Update webpack.mix.js

--- laravel-mix/laravel-mix/webpack.mix.js
+++ laravel-mix/laravel-mix/webpack.mix.js
@@ -1,17 +1,9 @@
 const mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.scripts([
+    'resources/js/my-script.js',
+    'resources/js/my-script-console.js',
+], 'public/js/my-app.js');
+mix.styles([
+    'resources/css/my-style.css',
+    'resources/css/my-style-h1.css',
+], 'public/css/my-app.css');
