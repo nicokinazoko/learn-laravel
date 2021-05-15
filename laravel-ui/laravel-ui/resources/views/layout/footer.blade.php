83b09968388212fc82fa918789c6de3ba4b31fe2
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-05-15 16:35:09 +0700

    Create footer.blade.php

---
+++ laravel-ui/laravel-ui/resources/views/layout/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="bg-dark py-4 text-white mt-4">
+    <div class="container">
+        Sistem Informasi Mahasiswa | Copyright © {{ date('Y') }} Duniailkom
+    </div>
+</footer>
+
+</body>
+
+</html>
