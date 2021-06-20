d5bcf3978c9b617fa64c3de3cd540ac6ef1e7b05
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-06-20 16:46:29 +0700

    Update web.php

--- collection/collection/routes/web.php
+++ collection/collection/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CollectionController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/satu', [CollectionController::class, 'collectionSatu']);
+Route::get('/dua', [CollectionController::class, 'collectionDua']);
+Route::get('/tiga', [CollectionController::class, 'collectionTiga']);
+Route::get('/empat', [CollectionController::class, 'collectionEmpat']);
+Route::get('/lima', [CollectionController::class, 'collectionLima']);
+Route::get('/enam', [CollectionController::class, 'collectionEnam']);
